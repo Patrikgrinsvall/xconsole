51a911a96024d03508d050402dc8a18a6c595779
Patrik <patrik.grinsvall@gmail.com>
2022-03-01 04:33:01 +0100

    fix

--- src/ServiceProviders/FileWatcher.php
+++ src/ServiceProviders/FileWatcher.php
@@ -2,37 +2,101 @@
 
 namespace PatrikGrinsvall\XConsole\ServiceProviders;
 
+use Exception;
+use SplFileInfo;
+use Throwable;
+
 class FileWatcher
 {
-    private static  $i = null;
-    public array    $paths;
-    protected array $callables;
 
-    public static function make(string $path = null, callable $callback = null)
+    private static self $i;
+    public int          $graceTime;                                // seconds to at least wait before checking file, to save disk and maybe cpu
+    public bool         $use_exceptions = false;                   // whethere or not to throw exceptions
+    public null|object  $event          = null;                    // an event to dispatch or null / false if no event
+    protected array     $paths;                                    // internal metadata, array with paths and their modified time
+    private array       $excluded       = [ '.', '..', '.git', ];
+    private int         $lastCheckTime  = 0;
+
+    public function __construct($paths = null, $gracetime = 0)
     {
-        if (self::$i !== null)
-        {
-            if ($path != null)
-            {
-                self::$i->add($path, $callback);
-            }
-
-            return self::$i;
-        }
-        self::$i = new static();
-        self::$i->add($path, $callback);
-
-        return self::$i;
+        $this->gracetime = $gracetime;
+        if ($paths != null) $this->add($paths);
     }
 
-    public function add(string $path, ?callable $callback = null)
+    /**
+     * Adds the path to a file or directory and a callback to run in case any file in path is changed
+     * since last iteration.
+     *
+     * @param string|array  $paths
+     * @param callable|null $callback
+     * @return $this
+     * @throws Exception
+     */
+    public function add(string|array|null $paths, null|callable $callback = null): static
     {
-        $stat = stat($path);
-
-        $last_mtime         = $stat['mtime'];
-        $this->paths[$path] = [ 'path' => $path, 'last_mtime' => $last_mtime, 'callback' => $callback, ];
+        if (is_null($paths)) return $this;
+        $pathsToAdd = is_string($paths) ? [ $paths ] : $paths;
+        foreach ($pathsToAdd as $path) {
+            foreach ($this->get_files($path) as $file) {
+                $this->paths[$file] = [ 'path' => $file, 'last_mtime' => 0, 'callback' => $callback, ];
+            }
+        }
 
         return $this;
+    }
+
+    /**
+     * returns an array with absolut filenames
+     * @param string|null $path
+     * @return array
+     * @throws Exception
+     * @todo move to flysystem to support remote watching
+     */
+    public function get_files(string|null $path): array
+    {
+        $info  = new SplFileInfo($path);
+        $files = match ($info->getType()) {
+            "dir"  => array_filter(scandir($path), function ($file) {
+                if (in_array($file, $this->excluded)) {
+                    return false;
+                }
+                if (is_file($file)) return true;
+
+                return false;
+            }),
+            "file" => [ $path ],
+
+        };
+
+        return $files;
+    }
+
+    /**
+     * Create an instance of filewatcher with optional callback to run if any files are changed.
+     * @param string|null   $path     - File or directory to watch.
+     * @param callable|null $callback - callback to run if files are changed in $path
+     * @return FileWatcher - returns an instance, run update to check if files are changed since last iteration
+     * @throws Exception
+     */
+    public static function make(string|array $path = null, callable $callback = null): static
+    {
+        if (!isset(self::$i)) {
+            self::$i            = new static();
+            self::$i->graceTime = 0;
+        }
+
+        return self::$i->add($path, $callback);
+    }
+
+    public function get_watched()
+    {
+        return $this->paths;
+    }
+
+    public function reset()
+    {
+        unset($this->paths);
+        $this->paths = [];
     }
 
     /**
@@ -40,58 +104,99 @@ class FileWatcher
      */
     public function stats(): array
     {
-        return $this->paths;
+        $output = [];
+        foreach ($this->paths as $path) {
+            $output = [ 'path' => $path['path'], 'last_mtime' => date('ymd h:i:s', $path['last_mtime']), ];
+        }
+
+        return $output;
     }
 
     /**
      * @param array $files
      */
-    public function watch(string ...$path)
+    public function watch(string|array ...$paths)
     {
-        foreach ($path as $item) $this->paths[] = $path;
+        foreach ($paths as $path) {
+            $this->add($path);
+        }
 
         return $this;
 
     }
 
-    public function get_changes()
+    public function update()
     {
-        $changes = null;
-        foreach ($this->paths as $path)
-        {
-            if ($this->changed($path['path']))
-            {
-                $changes[] = $path['path'];
-            }
+        $this->count_changes();
 
-        }
-        if (!isset($changes) || count($changes) == 0) return [];
-
-        return count($changes) == 1 ? $changes[0] : implode("\n", $changes);
-
+        return $this;
     }
 
     /**
+     * count changes since last check
+     * @return false|int|mixed
+     * @throws Throwable
+     */
+    public function count_changes()
+    {
+        if ($this->grace()) {
+            #  $this->throw_dispatch($this->grace(), 'RuntimeException, graceperiod');
+
+            return 0;
+        }
+        $changes = 0;
+
+        foreach ($this->paths as $path) {
+            $changes = $changes + $this->changed($path['path']);
+        }
+
+        return $changes + 1;
+    }
+
+    /**
+     * returns true if we are in a graceperiod
+     * @param int $graceTime - Time before we can check filesystem again, default to 5
+     * @return bool true if filesystem is resting, false if its ok to check it
+     */
+    public function grace(int $time = 5): bool
+    {
+
+        if ($this->lastCheckTime == 0) {
+            $this->lastCheckTime = time();
+
+            return true;
+        }
+
+
+        if (!isset($this->graceTime) || $time != 5) $this->graceTime = $time;
+
+        return ($time + $this->graceTime > time());
+    }
+
+    /**
+     * Returns false if path is not registred or not changed, true if changed since last check and
+     * if callback for that path is registred when file is added, also runs the callback.
+     *
      * @param string $path
      * @return int
      */
     public function changed(string $path)
     {
-        if (!isset($this->paths[$path]))
-        {
-
+        if (!isset($this->paths[$path])) {
             return 0;
         }
-        $last = $this->paths[$path]['last_mtime'];
-        clearstatcache(false, $path);
-        $stat = stat($path);
-        $now  = $stat['mtime'];
 
-        if ($last != $now)
-        {
+        $last = $this->paths[$path]['last_mtime'];
+
+        if (file_exists($path)) {
+            clearstatcache(true, $path);
+            $now = filemtime($path);
+        } else return 1;
+
+        if ($last != $now) {
             $this->paths[$path]['last_mtime'] = $now;
             $cb                               = $this->paths[$path]['callback'] ?? function ($item) {
-                    return false;
+                    return 0;
                 };
 
             $cb($path);
@@ -103,15 +208,32 @@ class FileWatcher
 
     }
 
-    public function count_changes()
+    /**
+     * @throws Throwable
+     */
+    public function throw_dispatch($condition, $message)
     {
-        $changes = 0;
-        foreach ($this->paths as $path)
-        {
-            $changes = $changes + $this->changed($path['path']);
+        error_log($message);
+        throw_if($condition && $this->use_exceptions, new Exception($message));
+        $this->dispatch_event_if($condition && $this->event !== null, $message);
 
+    }
+
+    public function dispatch_event_if($condition, $message)
+    {
+        if (is_object($this->event) && method_exists($this->event, 'dispatch')) {
+            $this->event::dispatch($message);
         }
+    }
 
+    public function get_changes()
+    {
+        $changes = [];
+        foreach ($this->paths as $path) {
+            if ($this->changed($path['path'])) {
+                $changes[] = $path['path'];
+            }
+        }
 
         return $changes;
 
--- tests/unit/FileWatcherTest.php
+++ tests/unit/FileWatcherTest.php
@@ -2,6 +2,7 @@
 
 namespace PatrikGrinsvall\XConsole\Tests;
 
+use PatrikGrinsvall\XConsole\ServiceProviders\FileWatcher;
 use PHPUnit\Framework\TestCase;
 
 class FileWatcherTest extends TestCase
@@ -9,11 +10,10 @@ class FileWatcherTest extends TestCase
     public function test_callbacks_without_changes()
     {
         $tempFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . date("ymdhis") . ".tmp";
-        $f        = FileWatcher::make($tempFile, null);
+        touch($tempFile, time() + 10);
+        $f = FileWatcher::make($tempFile, null);
 
         file_put_contents($tempFile, "trivial data");
-
-        touch($tempFile, time() + 10);
         self::assertEquals(0, $f->count_changes(), "testing so no changed file");
 
         unlink($tempFile);
@@ -24,12 +24,12 @@ class FileWatcherTest extends TestCase
         $tempFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . date('ymdhis') . '.tmp';
         file_put_contents($tempFile, 'trivial data');
         $f = FileWatcher::make($tempFile, function ($file) use ($tempFile) {
-
             self::assertTrue($file == $tempFile);
-        });
+        }, 0);
         touch($tempFile, time() + 10);
         file_put_contents($tempFile, 'trivial dataasdasd');
-        self::assertTrue($f->count_changes() === 1, 'Testing so we have one changed file');
+
+        self::assertEquals(1, count($f->get_changes()), 'Testing so we have one changed file');
         unlink($tempFile);
     }
 
@@ -56,33 +56,43 @@ class FileWatcherTest extends TestCase
     {
         $tempdir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . date('ymdhis') . 'dir';
         mkdir($tempdir);
-        $tempfiles = [ rand(1, 9999) . '.file1',
-                       rand(1, 9999) . '.file2',
-                       rand(1, 9999) . '.file3',
-        ];
-        foreach ($tempfiles as $y) {
-
-            touch($tempdir . DIRECTORY_SEPARATOR . $y, time() + 10);
+        $tempfiles = [ $tempdir . DIRECTORY_SEPARATOR . rand(1, 9999) . '.file1',
+                       $tempdir . DIRECTORY_SEPARATOR . rand(1, 9999) . '.file2',
+                       $tempdir . DIRECTORY_SEPARATOR . rand(1, 9999) . '.file3' ];
+        foreach ($tempfiles as $tempfile) {
+            touch($tempfile, time() + 10);
         }
-        $called  ['calledtimes'] = 0;
 
-        $f = FileWatcher::make($tempdir, function ($file) use (&$called) {
-            dump([ 'asd',
-                   $called,
-            ]);
-            $called['calledtimes'] = 123;
+
+        $f = FileWatcher::make();
+        $f->reset();
+
+        self::assertEmpty($f->get_watched(), "Test so it works to remove all watched files");
+        $called = 0;
+        $f->add($tempdir, function () use (&$called) {
+            error_log("-:" . $called);
+            $called = 123;
         });
-
-
-        foreach ($tempfiles as $y) {
-            touch($y, time() + 10);
+        self::assertIsArray($f->get_watched(), 'Test so it works to remove all watched files');
+        foreach ($tempfiles as $tempfile) {
+            touch($tempfile, time() + 10);
+            file_put_contents($tempfile, "another data set", FILE_APPEND);
         }
+        error_log(print_R($f->get_changes(), 1));
+        error_log(print_R($tempfiles, 1));
+        $f->grace(0);
+        self::assertEquals(2, $f->count_changes(), "test so we detected changes");
 
-        $f->count_changes();
+        self::assertCount(3, $f->get_changes(), "tests so files arrays are equal");
         foreach ($tempfiles as $ff) unlink($tempdir . DIRECTORY_SEPARATOR . $ff);
         rmdir($tempdir);
-        self::assertEquals(1, 1);
         ## self::assertEquals(2, $called['calledtimes'], 'All 3 files found');
 
+    }
+
+    public function setUp(): void
+    {
+        // todo add logic
+        parent::setUp();
     }
 }
